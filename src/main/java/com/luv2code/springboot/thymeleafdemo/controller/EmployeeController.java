6a0fa40ec622190e286b5b8181de980eaf614bd7
BackIntoTheGame <sxlwork@outlook.com>
2020-03-13 00:23:07 +0800

    rewire employees data to DB data. added index page into static for redirection

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,40 +1,33 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
+    private EmployeeService employeeService;
 
-    // load employee data
-    private List<Employee> employees;
-
-    @PostConstruct
-    private void loadData(){
-        employees = new ArrayList<>();
-
-        Employee empl = new Employee(1, "Leslie", "Andrew", "lesAndre@luv2code.come");
-        Employee emp2 = new Employee(1, "emma", "Back", "em@luv2code.come");
-        Employee emp3 = new Employee(1, "Avril", "Lav", "avrilLav@luv2code.come");
-
-        employees.add(empl);
-        employees.add(emp2);
-        employees.add(emp3);
+    // since we only have one constructor
+    // @Autowire is optional
+    private EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
     }
 
     // add mapping for "/list"
 
     @GetMapping("/list")
     public String listEmployees(Model model){
+
+        // get employees from the db
+        List<Employee> employees = employeeService.findAll();
 
         // add to the spring model
         model.addAttribute("employees", employees);
